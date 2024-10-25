30d380789d9d868fd38019aa004f99b9d5be56c8
Dmitriy <ddpemvr@mail.ru>
2024-10-25 18:14:34 +0500

    Adaptation questions

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+const questionBtnActions = document.querySelectorAll(".questions__action");
+const questionOpenedBlocks = document.querySelectorAll(".questions__openedblock");
+
+
+for (var i = 0; i < questionBtnActions.length; i++) {
+    questionBtnActions[i].addEventListener('click', fun_open);
+  
+    function fun_open(event) {
+      for (var i = 0; i < questionBtnActions.length; i++) {
+        if (questionBtnActions[i] == event.currentTarget) {
+            if(questionOpenedBlocks[i].classList.contains('questions__openedblock--show')) {
+                questionOpenedBlocks[i].style.height = 0;
+                questionOpenedBlocks[i].classList.remove('questions__openedblock--show');
+            } else {
+                questionOpenedBlocks[i].classList.add('questions__openedblock--show');
+                questionOpenedBlocks[i].style.height = questionOpenedBlocks[i].scrollHeight + 30 + 'px';
+            }
+        }
+      }
+  
+    }
+  
+  }
+
+  var typed = new Typed('.header__number', {
+    strings: ['+7-(900)-000-00-00'],
+    typeSpeed: 100,
+  });
